dfc78c43e2502e90026041274e64106632271939
EliteNoob <elitenoob.work@mail.ru>
2025-02-18 14:03:08 +0300

    fix encoding

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ logging.critical("A message of CRITICAL severity")
 
 description = '''PrimateKing'''
 
-with open("config.yml", esend_telegram_notificationncoding='utf-8') as f:
+with open("config.yml", encoding='utf-8') as f:
     config = yaml.load(f, Loader=yaml.FullLoader,)
 
 
